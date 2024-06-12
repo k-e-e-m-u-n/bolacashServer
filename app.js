a137dbb6b83b1ec442b85977b21c73fc1d4adc88
Gerald-Rotty <rotgakgoyit80@gmail.com>
2024-06-12 12:48:05 +0100

    update link

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors({origin: "*"}))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,application/json'); // Allow specific headers
     next();
   });
 
