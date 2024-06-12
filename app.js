8e6a970153cc681519cf324426074aed7de11e80
Gerald-Rotty <rotgakgoyit80@gmail.com>
2024-06-12 17:00:17 +0100

    update link

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(cors({
  
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use('/bolacash', router)
+app.use('/api/bolacash', router)
 
 // Creating the start server method
 const startServer  = async () => {
